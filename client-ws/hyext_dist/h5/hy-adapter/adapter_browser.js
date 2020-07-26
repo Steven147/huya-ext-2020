18dbced55c90bc69877cc2eb1e91275d4be0f648
LinShaoQin <41386916+Steven147@users.noreply.github.com>
2020-07-27 02:32:09 +0800

    update client

--- client-ws/hyext_dist/h5/hy-adapter/adapter_browser.js
+++ client-ws/hyext_dist/h5/hy-adapter/adapter_browser.js
@@ -1,14 +1,12 @@
-
-var require = function (src) {
-    var s = document.createElement('script');
-    s.async = false;
-    s.src = src;
-    s.addEventListener('load', function () {
-        s.parentNode.removeChild(s);
-        s.removeEventListener('load', arguments.callee, false);
-    }, false);
-    document.body.prepend(s);
+var require = function require(src) {
+  var s = document.createElement('script');
+  s.async = false;
+  s.src = src;
+  s.addEventListener('load', function () {
+    s.parentNode.removeChild(s);
+    s.removeEventListener('load', arguments.callee, false);
+  }, false);
+  document.body.prepend(s);
 };
 
 require('hy-adapter/adapter_base.js');
-    
--- client-ws/hyext_dist/h5/hy-main.js
+++ client-ws/hyext_dist/h5/hy-main.js
@@ -1,14 +1,11 @@
-
 //Runtime环境下面需要先加载adapter_base.js
-console.log('navigator.userAgent:',navigator.userAgent);
-if (window['navigator'] == undefined || window['navigator'] == null || navigator.userAgent.toLowerCase().indexOf('huya') >=0 ) {
-    require('hy-adapter/adapter_base.js');
-}
+console.log('navigator.userAgent:', navigator.userAgent);
 
-    
-//加载资源
-hyAdapterLoader.loadImages([]);
-        
-//加载脚本
+if (window['navigator'] == undefined || window['navigator'] == null || navigator.userAgent.toLowerCase().indexOf('huya') >= 0) {
+  require('hy-adapter/adapter_base.js');
+} //加载资源
+
+
+hyAdapterLoader.loadImages([]); //加载脚本
+
 hyAdapterLoader.loadScripts(["./pixi/pixi.min.js", "./src/main.js", "js/total.js"]);
-        
